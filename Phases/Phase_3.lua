2323d8f954471237a12a9b9cefe1fa02be3ab952
Arnaud Debray <debray.arnaud@gmail.com>
2014-10-18 01:09:54 +0200

    auto-home push

--- Phases/Phase_3.lua
+++ Phases/Phase_3.lua
@@ -1,104 +1,101 @@
 Phase = {}
 
-function Phase:quit()
-	love.event.quit()
-end
-
-function Phase:ft_left()
-	if self.images[1].effect_name == 'center' then
-		self.images[1].x = self.images[1].x + 128
-		self.images[1].effect_name = 'left'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
-	elseif self.images[1].effect_name == 'left' then
-		self.images[1].x = self.images[1].x - 256
-		self.images[1].effect_name = 'right'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
-	elseif self.images[1].effect_name == 'right' then
-		self.images[1].x = self.images[1].x + 128
-		self.images[1].effect_name = 'center'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
+function Phase:keydown()
+	self.bt_position = self.bt_position + 1
+	if self.bt_position > self.bt_position_max then
+		self.bt_position = 1
 	end
 end
 
-function Phase:ft_right()
-	if self.images[1].effect_name == 'center' then
-		self.images[1].x = self.images[1].x - 128
-		self.images[1].effect_name = 'right'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
-	elseif self.images[1].effect_name == 'right' then
-		self.images[1].x = self.images[1].x + 256
-		self.images[1].effect_name = 'left'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
-	elseif self.images[1].effect_name == 'left' then
-		self.images[1].x = self.images[1].x - 128
-		self.images[1].effect_name = 'center'
-		self.images[1].effect:send(
-			"img",
-			self.shaders[self.images[1].effect_name]
-		)
+function Phase:keyup()
+	self.bt_position = self.bt_position - 1
+	if self.bt_position < 1 then
+		self.bt_position = self.bt_position_max
 	end
+end
+
+function Phase:getsaves()
+	local state1 = love.filesystem.exists('save1')
+	local state2 = love.filesystem.exists('save2')
+	local state3 = love.filesystem.exists('save3')
+
+	return state1, state2, state3
 end
 
 function Phase:init()
+	local message1
+	local message2
+	local message3
+	local state1, state2, state3 = self:getsaves()
+
+	if state1 == true then
+		message1 = 'A game is saved here'
+	else
+		message1 = 'Free slot'
+	end
+
+	if state2 == true then
+		message2 = 'A game is saved here'
+	else
+		message2 = 'Free slot'
+	end
+
+	if state3 == true then
+		message3 = 'A game is saved here'
+	else
+		message3 = 'Free slot'
+	end
+
+
 	self.id = 3
-	self.name = 'New_Game'
-	self.images = {
+	self.name = 'New_save'
+	self.texts = {
 		{
-			image = love.graphics.newImage('Images/HeroSelection.png'),
-			effect_name = 'center',
-			effect = love.graphics.newShader [[
-				extern Image img;
-				vec4 effect(vec4 color,Image tex,vec2 tc,vec2 pc)
-				{
-					vec4 img_color = Texel(tex,tc);
-					vec4 trans_color = Texel(img,tc);
-
-					number white_level   = (
-						trans_color.r + trans_color.g + trans_color.b
-						) / 3;
-					number max_white   = 1;
-
-					if (white_level >= max_white)
-						return img_color;
-					else
-						{
-							img_color.a = 0;
-							return img_color;
-						}
-				}
-			]],
-			scale = 1,
-			x = 128,
-			y = 128
+			string = message1,
+			x = 100,
+			y = 100
+		},
+		{
+			string = message2,
+			x = 100,
+			y = 200
+		},
+		{
+			string = message3,
+			x = 100,
+			y = 300
+		}
+	}
+	self.bt_position = 1
+	self.bt_position_max = 3
+	self.shapes = {
+		{
+			shape = 'rectangle',
+			x = 100,
+			y = 100,
+			width = 50,
+			height = 50
+		},
+		{
+			shape = 'rectangle',
+			x = 100,
+			y = 200,
+			width = 50,
+			height = 50
+		},
+		{
+			shape = 'rectangle',
+			x = 100,
+			y = 300,
+			width = 50,
+			height = 50
 		}
 	}
 	self.kb_events = {
-		escape = self.quit,
-		left = self.ft_left,
-		right = self.ft_right
-	}
-	self.shaders = {
-		left = love.graphics.newImage('Images/HeroSelectionLeft.png'),
-		center = love.graphics.newImage('Images/HeroSelectionCenter.png'),
-		right = love.graphics.newImage('Images/HeroSelectionRight.png')
+		up = self.keyup,
+		down = self.keydown
 	}
 
-	self.images[1].effect:send("img", self.shaders[self.images[1].effect_name])
 end
 
 return Phase
---
+++ Phases/Phase_5.lua
@@ -0,0 +1,105 @@
+Phase = {}
+
+function Phase:quit()
+	love.event.quit()
+end
+
+function Phase:ft_left()
+	if self.images[1].effect_name == 'center' then
+		self.images[1].x = self.images[1].x + 128
+		self.images[1].effect_name = 'left'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	elseif self.images[1].effect_name == 'left' then
+		self.images[1].x = self.images[1].x - 256
+		self.images[1].effect_name = 'right'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	elseif self.images[1].effect_name == 'right' then
+		self.images[1].x = self.images[1].x + 128
+		self.images[1].effect_name = 'center'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	end
+end
+
+function Phase:ft_right()
+	if self.images[1].effect_name == 'center' then
+		self.images[1].x = self.images[1].x - 128
+		self.images[1].effect_name = 'right'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	elseif self.images[1].effect_name == 'right' then
+		self.images[1].x = self.images[1].x + 256
+		self.images[1].effect_name = 'left'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	elseif self.images[1].effect_name == 'left' then
+		self.images[1].x = self.images[1].x - 128
+		self.images[1].effect_name = 'center'
+		self.images[1].effect:send(
+			"img",
+			self.shaders[self.images[1].effect_name]
+		)
+	end
+end
+
+function Phase:init()
+	self.id = 5
+	self.name = 'New_Game'
+	self.images = {
+		{
+			image = love.graphics.newImage('Images/HeroSelection.png'),
+			effect_name = 'center',
+			effect = love.graphics.newShader [[
+				extern Image img;
+				vec4 effect(vec4 color,Image tex,vec2 tc,vec2 pc)
+				{
+					vec4 img_color = Texel(tex,tc);
+					vec4 trans_color = Texel(img,tc);
+
+					number white_level   = (
+						trans_color.r + trans_color.g + trans_color.b
+						) / 3;
+					number max_white   = 1;
+
+					if (white_level >= max_white)
+						return img_color;
+					else
+						{
+							img_color.a = 0;
+							return img_color;
+						}
+				}
+			]],
+			scale = 1,
+			x = 128,
+			y = 128
+		}
+	}
+	self.kb_events = {
+		escape = self.quit,
+		left = self.ft_left,
+		right = self.ft_right
+	}
+	self.shaders = {
+		left = love.graphics.newImage('Images/HeroSelectionLeft.png'),
+		center = love.graphics.newImage('Images/HeroSelectionCenter.png'),
+		right = love.graphics.newImage('Images/HeroSelectionRight.png')
+	}
+
+	-- Shader linking
+	self.images[1].effect:send("img", self.shaders[self.images[1].effect_name])
+end
+
+return Phase
